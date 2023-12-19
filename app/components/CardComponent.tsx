789349795006217c5dbd8a2c23e523f0d15e0def
razzuSharma <razzuSharma008@gmail.com>
2023-12-19 22:39:43 +0545

    features:adding new components

---
+++ app/components/CardComponent.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+
+interface Game {
+  name: string;
+  image: string;
+  link: string;
+}
+
+interface CardComponentProps {
+  data: Game[];
+}
+
+const CardComponent: React.FC<CardComponentProps> = ({ data }) => {
+  return (
+    <main
+      className="bg-cover bg-center min-h-screen bg-green-900"
+      style={{ backgroundImage: 'url("/background-image.jpg")' }}
+    >
+      <div className="container mx-auto py-12">
+        <h1 className="text-4xl text-center font-bold text-white mb-8">
+          🎰 Welcome to our Games 🎲
+        </h1>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {data.map((game: Game, index: number) => (
+            <div
+              key={index}
+              className="group px-10 py-5 bg-gradient-to-b from-green-800 to-green-700 rounded-lg flex flex-col items-center justify-center gap-2 relative after:absolute after:h-full after:bg-[#abd373] z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0 [&amp;_p]:delay-200 [&amp;_p]:transition-all"
+            >
+              <div
+                className="w-44 h-44 card1img aspect-square text-[#abd373] group-hover:bg-gray-800 text-5xl rounded-full p-2 transition-all duration-300 group-hover:transition-all group-hover:duration-300 group-hover:-translate-y-2 mx-auto"
+                style={{
+                  background: `radial-gradient(circle at 50% 50%, transparent 70%, #abd373), url(${game.image}) center/cover`,
+                }}
+              ></div>
+              <p className="cardtxt text-gray-200 tracking-wider group-hover:text-gray-700 text-xl font-bold">
+                {game.name}
+              </p>
+
+              <div className="ordernow flex flex-row justify-between items-center w-full">
+                <a
+                  href={game.link}
+                  className="btun4 lg:inline-flex items-center gap-3 group-hover:bg-white/10 bg-[#abd373] shadow-[10px_10px_150px_#ff9f0d] cursor-pointer py-2 px-4 text-sm font-semibold rounded-full butn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Visit Game
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default CardComponent;
--- app/components/WelcomCard.tsx
+++ app/components/WelcomCard.tsx
@@ -1,34 +1,35 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 
 const WelcomeCard = () => {
   return (
     <main className="flex items-center justify-center min-h-screen">
       <div className="text-center">
-        <h1 className="text-8xl font-bold mb-10 relative">
+        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold mb-5 md:mb-8 relative">
           Welcome to{" "}
           <span className="border-b-2 border-teal-500">Fast Gaming</span>
         </h1>
-        <p className="text-xl font-semibold text-gray-500 mb-4">
+        <p className="text-base md:text-lg lg:text-xl font-semibold text-gray-500 mb-3 md:mb-4">
           Play Smarter, Play Harder, Play With Fast Gaming
         </p>
-        <p className="text-white mb-6">
+        <p className="text-sm md:text-base text-white mb-4 md:mb-6">
           Immerse yourself in a world of thrilling games and interactive
           experiences, where the excitement never ends.
         </p>
-        <p className="text-white mb-6">
+        <p className="text-sm md:text-base text-white mb-4 md:mb-6">
           Join our vibrant community of gamers and enjoy the latest innovations
           in online gaming.
         </p>
-        <p className="text-lg font-bold text-yellow-400 mb-4">
+        <p className="text-xs md:text-sm  lg:text-lg font-bold text-yellow-400 mb-2 md:mb-4 sm:mb-10">
           Spin, Bet, Win - Experience the Thrill of Fast Gaming!
         </p>
-        <button
-          className="bg-transparent border border-yellow-400 text-white py-2 px-4 rounded-md transition-all duration-300 hover:bg-yellow-400 hover:text-black"
-          onClick={() => alert("Button Clicked")} // Replace with your desired action
+        <Link
+          className="bg-transparent border  border-yellow-400 text-white py-2 px-4 rounded-md text-sm md:text-base transition-all duration-300 hover:bg-yellow-400 hover:text-black"
+          href="/games-list" // Replace with your desired action
         >
           Join the Fun Now!
-        </button>
+        </Link>
       </div>
     </main>
   );
