7222c2fe48c5a8332f6b2a481c4c3c0a676da544
razzuSharma <razzuSharma008@gmail.com>
2023-12-19 23:00:26 +0545

    features:added search icons and search functionality

--- app/components/CardComponent.tsx
+++ app/components/CardComponent.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa"; // Import the back button icon
+import { FaSearch, FaArrowLeft } from "react-icons/fa"; // Import the back button icon
 interface Game {
   name: string;
   image: string;
@@ -13,21 +13,40 @@ interface CardComponentProps {
 }
 
 const CardComponent: React.FC<CardComponentProps> = ({ data }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredData = data.filter((game) =>
+    game.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main
       className="bg-cover bg-center min-h-screen bg-green-900"
       style={{ backgroundImage: 'url("/background-image.jpg")' }}
     >
       <div className="container mx-auto py-12">
-        <Link href="/" className="flex items-center text-white mb-4">
-          <FaArrowLeft className="mr-2" />
-        </Link>
+        <div className="flex items-center justify-between mb-8">
+          <Link href="/" className="flex items-center text-white">
+            <FaArrowLeft className="mr-2 text-2xl" />
+            <span className="text-lg font-semibold">Back</span>
+          </Link>
+          <div className="relative flex items-center">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent border-b border-white text-white pl-4 pr-8 py-2 focus:outline-none focus:border-[#abd373] transition-all duration-300"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <FaSearch className="text-white absolute right-2 cursor-pointer" />
+          </div>
+        </div>
         <h1 className="text-4xl text-center font-bold text-white mb-8">
-          🎰 Welcome to our Games 🎲
+          🎰 Our Games 🎲
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {data.map((game: Game, index: number) => (
+          {filteredData.map((game: Game, index: number) => (
             <div
               key={index}
               className="group px-10 py-5 bg-gradient-to-b from-green-800 to-green-700 rounded-lg flex flex-col items-center justify-center gap-2 relative after:absolute after:h-full after:bg-[#abd373] z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0 [&amp;_p]:delay-200 [&amp;_p]:transition-all"
